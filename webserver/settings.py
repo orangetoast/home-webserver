43abcbec7f7170f26624164e0bb48dcf299bb436
orangetoast <toast9096@gmail.com>
2022-08-17 20:57:18 +0900

    Feat: Add main page

--- webserver/settings.py
+++ webserver/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "portfolio",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -119,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
