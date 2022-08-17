2cbd6f161bcbc62e3ad527d81b3f30e57ef4cd0f
Jaeyeon Kim <fobbb77@gmail.com>
2022-08-17 10:30:41 +0900

    Feat: Add project model

--- projects/models.py
+++ projects/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class SkillStack(models.Model):
-    icon = models.ImageField(upload_to="icon/")
-    skill_name = models.CharField()
+    #    icon = models.ImageField(upload_to="icon/")
+    skill_name = models.CharField(max_length=50)
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=50)
+    skill_stack = models.ManyToManyField(SkillStack, blank=True, related_name="skills")
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
 
 
 class ImageSet(models.Model):
     image = models.ImageField(upload_to="project_image/")
-
-
-class Project(models.Model):
-    project_name = models.CharField()
-    skill_stack = models.ManyToManyField(SkillStack, blank=True, related_name="skills")
-    images = models.ForeignKey(ImageSet, on_delete=models.CASCADE, related_name="image_set")
-    description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
